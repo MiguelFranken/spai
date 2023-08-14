0079103e9a23a7f7108b80b23fe51e8623d9a7a9
mfranken <miguel.franken@denkwerk.com>
2023-08-14 17:25:49 +0200

    Remove redundant files

--- services/smart-personas-accessibility-reporter/src/app.module.ts
+++ services/smart-personas-accessibility-reporter/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import {A11yReportModule} from "./modules/a11y-report.module";
 
 @Module({
   imports: [A11yReportModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
