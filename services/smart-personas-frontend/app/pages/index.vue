c37c64c390784543b7fb5b27ec0ee044fe6080be
mfranken <miguel.franken@denkwerk.com>
2023-08-16 19:59:58 +0200

    Add frontend request to a11y checker

--- services/smart-personas-frontend/app/pages/index.vue
+++ services/smart-personas-frontend/app/pages/index.vue
@@ -1,22 +1,59 @@
 <script setup lang="ts">
-function foo() {
-  // eslint-disable-next-line no-console
-  console.log('Hello')
-}
+const url = ref('');
+const review = ref('');
+const error = ref('');
+
+const fetchReview = async () => {
+  try {
+    const response = await fetch(
+      //`http://review-generator.local/review?url=${url.value}`,
+      `http://localhost:3000/a11y-report?url=http://example.com`,
+    );
+    if (response.ok) {
+      const data = await response.json();
+      console.log("data", data)
+      review.value = data.review;
+      error.value = '';
+    } else {
+      error.value = 'Failed to generate review. Please try again later.';
+      console.error('Failed to generate review. Please try again later.')
+      review.value = '';
+    }
+  } catch (e) {
+    error.value = 'An error occurred while fetching the review!';
+    console.error(e)
+    review.value = '';
+  }
+};
 </script>
 
 <template>
-  <div class="bg-color-orange-400 p-8 space-y-6">
-    <div
-      class="bg-red-500 rounded-$radii-200 p-8"
-      outline="~ solid size-$size-100 color-$color-primary-hover-cta-background"
-      border="~ solid size-$size-50 color-black"
-    >
-      Hello World
-    </div>
+  <div class="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+    <div class="p-8 bg-white rounded-lg shadow-md w-96">
+      <h1 class="text-2xl font-semibold mb-4 text-blue-500">Accessibility Review</h1>
 
-    <button type="button" @click="foo">
-      hello world
-    </button>
+      <div class="flex flex-col space-y-4">
+        <input
+          v-model="url"
+          type="text"
+          placeholder="Enter URL"
+          class="p-2 border rounded"
+        />
+
+        <button
+          @click="fetchReview"
+          class="border-0 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 cursor-pointer"
+        >
+          Generate Review
+        </button>
+
+        <div v-if="review" class="text-green-500">
+          {{ review }}
+        </div>
+        <div v-if="error" class="text-red-500">
+          {{ error }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
