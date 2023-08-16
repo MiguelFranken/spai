d9905cc79b107f5f02113e61136723f52692be29
mfranken <miguel.franken@denkwerk.com>
2023-08-16 20:17:09 +0200

    Enable CORS in review generator

--- services/smart-personas-review-generator/src/main.py
+++ services/smart-personas-review-generator/src/main.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from api.routes import init_routes
 from api.error_handlers import init_error_handlers
 
 app = Flask(__name__)
+CORS(app)
+
 init_routes(app)
 init_error_handlers(app)
 
