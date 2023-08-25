2a4ed0e64dd3b798d3fbbd92494432c0a2c20940
mfranken <miguel.franken@denkwerk.com>
2023-08-25 20:30:51 +0200

    Show reviewed website stub

--- services/smart-personas-frontend/app/components/ReviewHeader.vue
+++ services/smart-personas-frontend/app/components/ReviewHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="relative w-full py-12 color-white overflow-hidden"
+    class="relative w-full color-white overflow-hidden"
     
   >
     <div class="absolute inset-0 overflow-hidden">
@@ -9,51 +9,59 @@
       />
     </div>
     
-    <LayoutContainer class="relative">
-      <div class="flex justify-between">
-        <!-- Persona -->
-        <div class="flex gap-8 items-center">
-          <div>
-            <div class="relative inline-block">
+    <div class="py-12">
+      <LayoutContainer class="relative">
+        <div class="flex justify-between">
+          <!-- Persona -->
+          <div class="flex gap-8 items-center">
+            <div>
+              <div class="relative inline-block">
+                <img
+                  class="h-40 w-40 rounded-full"
+                  src="~/assets/grandma.png"
+                  alt=""
+                />
+              </div>
+            </div>
+
+            <div class="space-y-2">
+              <div class="text-4xl font-pacifico">Claudia</div>
+              <ul class="list-disc list-inside">
+                <li>Grandmother with macular degeneration</li>
+                <li>Uses a screen magnifier and changes colors for contrast</li>
+                <li>Volunteers in social work</li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="flex gap-8 items-center">
+            <div>
               <img
-                class="h-40 w-40 rounded-full"
+                class="h-24 w-24 rounded-full"
                 src="~/assets/grandma.png"
                 alt=""
               />
+              <div class="text-center">John</div>
+            </div>
+
+            <div>
+              <img
+                class="h-24 w-24 rounded-full"
+                src="~/assets/grandma.png"
+                alt=""
+              />
+              <div class="text-center">John</div>
             </div>
           </div>
-          
-          <div class="space-y-2">
-            <div class="text-4xl font-pacifico">Claudia</div>
-            <ul class="list-disc list-inside">
-              <li>Grandmother with macular degeneration</li>
-              <li>Uses a screen magnifier and changes colors for contrast</li>
-              <li>Volunteers in social work</li>
-            </ul>
-          </div>
         </div>
-
-        <div class="flex gap-8 items-center">
-          <div>
-            <img
-              class="h-24 w-24 rounded-full"
-              src="~/assets/grandma.png"
-              alt=""
-            />
-            <div class="text-center">John</div>
-          </div>
-
-          <div>
-            <img
-              class="h-24 w-24 rounded-full"
-              src="~/assets/grandma.png"
-              alt=""
-            />
-            <div class="text-center">John</div>
-          </div>
-        </div>
-      </div>
-    </LayoutContainer>
+      </LayoutContainer>
+    </div>
+    
+    <div class="relative bg-black/10 h-12">
+      <LayoutContainer class="h-full flex items-center">
+        Reviewing website: https://thomann.de/
+      </LayoutContainer>
+    </div>
   </div>
 </template>
 
