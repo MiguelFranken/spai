9bba23cec8a09dd3cf19a256f67e8c6363a20627
mfranken <miguel.franken@denkwerk.com>
2023-08-26 16:57:19 +0200

    Eslint fix index.vue

--- services/smart-personas-frontend/app/pages/index.vue
+++ services/smart-personas-frontend/app/pages/index.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-const url = ref('');
+const url = ref('')
 
 const isValidURL = computed(() => {
   try {
     // eslint-disable-next-line no-new
     new URL(url.value)
     return true
-  } catch (e) {
+  }
+  catch (e) {
     return false
   }
 })
@@ -17,7 +18,9 @@ const reviewPage = computed(() => `/review/${encodeURIComponent(url.value)}`)
 <template>
   <div class="pt-32 flex items-center justify-center">
     <div class="p-8 bg-white rounded-lg shadow-md w-96">
-      <h1 class="text-2xl font-semibold mb-4 text-blue-500">Accessibility Reviews</h1>
+      <h1 class="text-2xl font-semibold mb-4 text-blue-500">
+        Accessibility Reviews
+      </h1>
 
       <div class="flex flex-col space-y-4">
         <input
@@ -25,14 +28,14 @@ const reviewPage = computed(() => `/review/${encodeURIComponent(url.value)}`)
           type="text"
           placeholder="Enter URL"
           class="p-2 border rounded"
-        />
+        >
 
         <NuxtLink
           :to="isValidURL ? reviewPage : undefined"
           class="border-0 text-center p-2 text-white rounded"
           :class="{
             'cursor-pointer bg-blue-500 hover:bg-blue-600': isValidURL,
-            'cursor-not-allowed bg-gray-300': !isValidURL
+            'cursor-not-allowed bg-gray-300': !isValidURL,
           }"
         >
           Generate Review
